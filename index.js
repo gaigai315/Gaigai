9ddf141044e0e638877fe0da7b46245454f8166a
gaigai315 <www.512204242@qq.com>
2025-11-21 14:45:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 记忆表格 v3.3.0
+// 记忆表格 v3.5.0
 (function() {
     'use strict';
     
@@ -8,9 +8,9 @@
     }
     window.GaigaiLoaded = true;
     
-    console.log('🚀 记忆表格 v3.3.0 启动');
+    console.log('🚀 记忆表格 v3.5.0 启动');
     
-    const V = 'v3.3.0';
+    const V = 'v3.5.0';
     const SK = 'gg_data';
     const UK = 'gg_ui';
     const PK = 'gg_prompts';
@@ -3907,6 +3907,7 @@ const h = `
     }, 500); // 延迟500毫秒确保 window.Gaigai 已挂载
 })();
 })();
+
 
 
 
